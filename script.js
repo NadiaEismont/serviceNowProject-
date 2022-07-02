9a8f3606dd58532262e5fd3cbb637153763db5b5
Nadia Nechuiviter <nadiaejsmont@gmail.com>
2022-07-02 16:59:27 +0100

    second

---
+++ script.js
@@ -0,0 +1,20 @@
+$(".carousel").carousel({
+  interval: 2000,
+});
+
+const spinner = document.querySelector(".loadingSpinner");
+const backdrop = document.querySelector(".backdrop");
+
+async function startSpinner() {
+  spinner.style.display = "block";
+  toggleModal();
+}
+
+function hideLoader() {
+  toggleModal();
+  spinner.style.display = "none";
+}
+
+function toggleModal() {
+  backdrop.classList.toggle("is-hidden");
+}
