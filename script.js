58ce7bae263c6183d2d21bbb425c641f53869d1c
Nadia Nechuiviter <nadiaejsmont@gmail.com>
2022-07-03 14:28:31 +0100

    22

--- script.js
+++ script.js
@@ -18,3 +18,13 @@ function hideLoader() {
 function toggleModal() {
   backdrop.classList.toggle("is-hidden");
 }
+
+async function start_stop_spinner() {
+  console.log("start timer");
+  await startSpinner();
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  hideLoader();
+  console.log("after 1 second");
+}
+
+start_stop_spinner();
